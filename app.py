00684eec02e6a32fac20ea1d5fd2a0d2d8babec1
ABHINAV JAIN <abhinavjain@ABHINAVs-MacBook-Air.local>
2021-04-09 04:09:28 +0100

    Heroku fix

--- app.py
+++ app.py
@@ -2,35 +2,39 @@ import pickle
 import numpy as np
 from flask import Flask, request
 
-model = None
-app = Flask(__name__)
-
 def load_model():
     global model
     with open('model_19900_70_C10_G0_train975.pkl', 'rb') as f:
         model = pickle.load(f)
 
+model = None
+app = Flask(__name__)
+load_model()
+
 @app.route('/')
 def home_endpoint():
-    return 'Mile End Spoken Numerals, Get a prediction via /predict-intonation endpoint.'
+    return 'Mile End Spoken Numerals, Get a prediction via /predict-intonation endpoint.', 200
 
 @app.route('/predict-intonation', methods=['GET'])
 def get_prediction():
 
     data = request.get_json()  
     data = np.array(data)[np.newaxis, :]
-    prediction = model.predict(data)  
+    try:
+        prediction = model.predict(data)  
+    except:
+        return ("Model could not be loaded"), 404
     
     if prediction[0]==1:
-        return ("Prediction for intonation is: "+"Question")
+        return ("Prediction for intonation is: "+"Question"), 200
     elif prediction[0]==2:
-        return ("Prediction for intonation is: "+"Excited")
+        return ("Prediction for intonation is: "+"Excited"), 200
     elif prediction[0]==3:
-        return ("Prediction for intonation is: "+"Neutral")
+        return ("Prediction for intonation is: "+"Neutral"), 200
     elif prediction[0]==4:                        
-        return ("Prediction for intonation is: "+"Bored")
+        return ("Prediction for intonation is: "+"Bored"), 200
     else:    
-        return ("Prediction for intonation is: "+"Unknown, or there may be an error")
+        return ("Prediction for intonation is: "+"Unknown, or there may be an error"), 404
 
 if __name__ == '__main__':
     load_model() 
