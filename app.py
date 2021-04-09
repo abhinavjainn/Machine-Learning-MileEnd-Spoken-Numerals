5e4d6e1771882d25fce290f007b4cbf673da5085
ABHINAV JAIN <abhinavjain@ABHINAVs-MacBook-Air.local>
2021-04-09 04:34:51 +0100

    Cosmetic changes

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import numpy as np
 from flask import Flask, request
 
 def load_model():
+    '''
+    Load the model from disk.
+    Retruns: Model instance    
+    '''
     global model
     with open('model_19900_70_C10_G0_train975.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -11,21 +15,33 @@ model = None
 app = Flask(__name__)
 load_model()
 
+# Endpoint: Home
 @app.route('/')
 def home_endpoint():
-    return 'Mile End Spoken Numerals, Get a prediction via /predict-intonation endpoint.', 200
+    return 'You are at home page of "Mile End Spoken Numerals". Get a prediction via /predict-intonation endpoint.', 200
 
+# Endpoint: predict-intonation
 @app.route('/predict-intonation', methods=['GET'])
 def get_prediction():
+    '''
+    Makes prediction for intonation using the input from json request.
+    Returns: Predicted intonation
+    '''
 
-    data = request.get_json()  
-    data = np.array(data)[np.newaxis, :]
+    # Get JSON request data
+    data = request.get_json() 
+    # Convert 1-d list input to 2-d numpy aaray 
+    model_input = np.array(data)[np.newaxis, :]
+    
     try:
-        prediction = model.predict(data)  
+        # Call the model instance to make predictions for intonation
+        prediction = model.predict(model_input)  
     except:
+        # Error if model instance not found
         return ("Model could not be loaded"), 404
     
-    if prediction[0]==1:
+    # Model instance found - Return prediction by mapping numeric label to corresponding intonation
+    if prediction[0]==1:  
         return ("Prediction for intonation is: "+"Question"), 200
     elif prediction[0]==2:
         return ("Prediction for intonation is: "+"Excited"), 200
@@ -36,6 +52,6 @@ def get_prediction():
     else:    
         return ("Prediction for intonation is: "+"Unknown, or there may be an error"), 404
 
+# Required for local testing
 if __name__ == '__main__':
-    load_model() 
     app.run(host='0.0.0.0')
